55baac2021f153aad381414b79ae304e176bd9aa
Kaushal Barhate <barhate.kaushal@gmail.com>
2023-08-11 22:06:21 +0530

    tsignore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-import Image from 'next/image'
 import EcoScriptIDE from './components/EcoScriptIDE';
-import Documentation from './components/Documentation';
 export default function Home() {
   return (
-    <>
 
      <EcoScriptIDE />
-     {/* <Documentation /> */}
-    </>
   )
 }
